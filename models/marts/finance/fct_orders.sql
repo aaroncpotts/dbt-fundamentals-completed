6be3145287a584a986dc7667463a26e01044a2de
aaroncpotts <aaroncpotts@gmail.com>
2025-03-04 11:27:34 -0500

    Cloning completed fundamentals tutorial
    Completed dbt fundamentals; archive of work.

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,30 @@
+with orders as  (
+    select * from {{ ref ('stg_jaffle_shop__orders' )}}
+),
+
+payments as (
+    select * from {{ ref ('stg_stripe__payments') }}
+),
+
+successful_payments as (
+    select
+        order_id,
+        sum (case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+    
+ final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce (successful_payments.amount, 0) as amount
+
+    from orders
+    left join successful_payments using (order_id)
+)
+select *
+from final
